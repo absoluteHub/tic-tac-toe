f415f38b1dbe9ac17419df4dbcdfa07fa9fbdeb7
Цап Андрій <145117753+absoluteHub@users.noreply.github.com>
2023-12-27 13:34:12 +0200

    Add method for user move
    Створення метода для хода гравця

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
     private static void printTableMapping() {
@@ -14,5 +16,38 @@ public class TicTacToe {
             System.out.println("|");
         }
         System.out.println("-------------");
+    }
+
+    private static void makeUserMove(char[][] gameTable) {
+        while (true) {
+            System.out.println("Please type number between 1 and 9:");
+            String string = new Scanner(System.in).nextLine();
+            if (string.length() == 1) {
+                char digit = string.charAt(0);
+                if (digit >= '1' && digit <= '9') {
+                    if (makeUserMoveToCell(gameTable, digit)) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean makeUserMoveToCell(char[][] gameTable, char digit) {
+        char[][] mappingTable = {{'7', '8', '9'}, {'4', '5', '6'}, {'1', '2', '3'}};
+        for (int i = 0; i < mappingTable.length; i++) {
+            for (int j = 0; j < mappingTable[i].length; j++) {
+                if (mappingTable[i][j] == digit) {
+                    if (gameTable[i][j] == ' ') {
+                        gameTable[i][j] = 'X';
+                        return true;
+                    } else {
+                        System.out.println("Can't make a move, because the cell is not free! Try again!");
+                        return false;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
