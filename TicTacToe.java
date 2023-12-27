061c72ea5424a8fc46432633016d05c6c9f6f5d3
Цап Андрій <145117753+absoluteHub@users.noreply.github.com>
2023-12-27 13:33:03 +0200

    Add print Table
    Створення таблиці, для відсліжування хода гри

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,2 +1,18 @@
 public class TicTacToe {
+
+    private static void printTableMapping() {
+        char[][] mappingTable = {{'7', '8', '9'}, {'4', '5', '6'}, {'1', '2', '3'}};
+        printGameTable(mappingTable);
+    }
+
+    private static void printGameTable(char[][] gameTable) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
+    }
 }
