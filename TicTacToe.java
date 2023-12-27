5a5dc4ddb92f243fbab4d45136fa5885f2f85b83
Цап Андрій <145117753+absoluteHub@users.noreply.github.com>
2023-12-27 13:36:17 +0200

    Add method main
    Створення головного метода для користування грою

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,6 +2,42 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9:");
+        printTableMapping();
+        char[][] gameTable = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        if (new Random().nextBoolean()) {
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+        }
+        while (true) {
+            makeUserMove(gameTable);
+            printGameTable(gameTable);
+            if (isUserWin(gameTable)) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+            if (isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+            if (isComputerWin(gameTable)) {
+                System.out.println("COMPUTER WIN!");
+                break;
+            }
+            if (isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!");
+    }
 
     private static void printTableMapping() {
         char[][] mappingTable = {{'7', '8', '9'}, {'4', '5', '6'}, {'1', '2', '3'}};
@@ -51,6 +87,7 @@ public class TicTacToe {
         }
         return false;
     }
+
     private static void makeComputerMove(char[][] gameTable) {
         Random random = new Random();
         while (true) {
@@ -62,6 +99,7 @@ public class TicTacToe {
             }
         }
     }
+
     private static boolean isUserWin(char[][] gameTable) {
         return isWinner(gameTable, 'X');
     }
